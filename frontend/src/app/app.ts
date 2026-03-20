5a66e3b681f0206d0318c384fdda33ff159d6841
Boris <b.grozdic96@gmail.com>
2026-03-20 10:01:49 +0100

    Setup frontend (Angular)

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App implements OnInit {
+  protected readonly title = signal('frontend');
+
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get('/api/experience').subscribe(console.log);
+  }
+}
